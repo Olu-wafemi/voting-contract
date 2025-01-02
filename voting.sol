8b0dc95470938b5fb27e4351cb692c90e8b9f600
Oluwafemi <femio82@gmail.com>
2025-01-02 22:46:15 +0100

    feat:function for assiginingvotes

--- voting.sol
+++ voting.sol
@@ -8,4 +8,45 @@ contract Ballot {
         bool has_voted;
         uint vote;
     }
+
+    address public admin;
+
+    struct Candidates {
+        bytes32 firstname;
+        bytes32 lastname;
+        uint age;
+        uint votecount;
+
+    }
+
+    mapping(address => Voter) public voters;
+
+    Candidates[] public candidates;
+
+    constructor(bytes32[] memory firstname, lastname, age){
+        admin = msg.sender
+        voters[admin].size = 1;
+
+        for (uint i = 0; i < candidateNames.length; i++){
+            candidates.push(Candidate({
+                firstname: firstname,
+                lastname: lastname,
+                age: age,
+                voteCount: 0
+            }))
+
+        }
+
+        
+    }
+
+    function assignvotingright(address voter_address) external{
+
+
+        require(msg.sender == admin);
+        require(!voters[voter_address].voted);
+        require(voters[voter_address].weight == 0);
+        voters[voter_address].weight = 1
+
+    }
 }
